5c088e0073234b7d2af4e6278ba2896527c253f1
manu <manu@Nuney-PC>
2021-08-07 11:41:45 +0530

    Added dao

--- SpringBootDemo/src/main/java/com/sprint/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/sprint/SpringBootDemoApplication.java
@@ -8,6 +8,7 @@ public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		// my name is priyansh
 	}
 
 }
---
+++ SpringBootDemo/src/main/java/com/sprint/dao/DemoInterface.java
@@ -0,0 +1,7 @@
+package com.sprint.dao;
+
+public interface DemoInterface {
+	public default void Display() {
+	}
+
+}
